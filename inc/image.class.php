c2e9f20fbbc6aa14355730fb9477172a3bc067b9
Sean Nessworthy <bladescope@googlemail.com>
2013-01-26 22:56:16 +0000

    More work on core class. Renaming like a boss.

--- inc/image.class.php
+++ inc/image.class.php
@@ -1,9 +1,12 @@
 <?php
 
-	class Image {
+	abstract class Image {
 			
 		// The base URL used to access the image site.
 		protected $base_url;
+		
+		// The image hash. Overwrite generate_hash() to modify this.
+		protected $raw_hash;
 		
 		
 		
@@ -22,7 +25,7 @@
 		protected function generate_hash();
 		
 		/**
-		 * Checks if a URL exists.
+		 * Checks if a URL exists by pinging it via. cURL.
 		 * 
 		 * @param	string	$uri	The uri to check.
 		 * @return	boolean			False on failure to validate, true if successful.
@@ -47,15 +50,34 @@
 		 * @param	string	$uri	The uri to check.
 		 * @return	boolean			False on failure to validate, true if successful.
 		 */
-		protected function validate_image($url);
+		public function validate_image($url);
 		
 		/**
 		 * Returns an image URL.
 		 */
-		public function getImage();
-		
-		public function __construct() {
+		final public function getImage() {
 			
+			// Validation!
+			$hash = $this->raw_hash;
+			$url;
+			
+			
+			if($this->validate_image($hash)) {
+				
+				return $url;
+				
+			}
+			
+			
+		}
+		
+		public function __construct($hash=null) {
+			
+			if(!$hash) {
+				$hash = $this->generate_hash();
+			}
+			
+			$this->raw_hash = $hash;
 		}
 		
 	}
